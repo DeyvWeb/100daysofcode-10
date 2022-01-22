fddbb1bff89bc73088df7d6fa85554876aa3aa9f
Deyvant R_K_Bhardwaj <58858873+DeyvWeb@users.noreply.github.com>
2022-01-22 23:17:51 +0530

    Add files via upload

---
+++ HarryEx4.java
@@ -0,0 +1,46 @@
+
+// 1. Write a java program to convert a string to lowercase
+// 2. Write a Java program to replace spaces with underscores
+// 3. Write a java program to fill in a letter template whic
+
+
+// letter = "Dear<|name|>, Trans a lot"
+// Replace <|name|> with a string (some name)
+
+// 4. Write a java program to detect double and triple spaces in a string.
+// 5. Write a program to format the following better using escape sequence characters.
+
+
+public class HarryEx4 {
+    
+    // public static void main(String[] args) {
+    
+    // String str = new String("DEYVant");
+    // System.out.println(str.toLowerCase());   
+    // }
+
+// public static void main(String[] args) {
+//     String str = new String("Add Underscore");
+//     System.out.println(str.replace("", "_"));
+    
+// }
+
+// public static void main(String[] args) {
+//     String str = new String("Dear <|name|>, Thanks a lot");
+//     System.out.println(str.replace("<|name|>", "Deyvant"));
+    
+// }
+
+// public static void main(String[] args) {
+//     String str = new String("This string contains double and  triple spaces");
+//     System.out.println(str.indexOf("  "));
+//     System.out.println(str.indexOf("   "));
+    
+// }
+public static void main(String[] args) {
+    String myLetter = "Dear Harry,\n\tThis Java Course is Nice.\nThanks!";
+        System.out.println(myLetter);
+    
+}
+
+}
